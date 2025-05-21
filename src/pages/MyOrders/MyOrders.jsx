a850b88805b7fe1b8a80c35574a28fdac8216249
laxmandas77 <laxmandaspanchireddy9@gmail.com>
2025-05-21 16:05:20 +0530

    fodds

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -9,7 +9,7 @@ const MyOrders = () => {
    const[data,setData] = useState([]);
 
    const fetchOrders = async () => {
-    const response = await axios.get('http://localhost:8080/api/orders', { headers: { 'Authorization': `Bearer ${token}`}});
+    const response = await axios.get('https://foodcustomerdashboard.netlify.app/api/orders', { headers: { 'Authorization': `Bearer ${token}`}});
     setData(response.data);
     console.log(response.data);
    };
